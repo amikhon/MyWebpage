91db54b337eacdf6de00481ad366e087caa230a0
amikhon <amikhon@email.ecok.edu>
2017-03-02 17:10:23 -0600

    My Webpage
    I created this in Spring 2017

---
+++ AboutMe.js
@@ -0,0 +1,7 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='AboutMeHome.html'>Home</a></li>" +
+"<li><a href='https://amiehoneycutt.weebly.com'>My Website</a></li>" +
+"<li><a href='AboutMe.html'>About Me</a></li>" +
+"<li><a href='MockCal.html'>Calendar</a></li>" +
+"</ul>";
